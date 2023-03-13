4ff0838ca7d7b2bd8e438cd6e5d31f9b94f885ba
hillarydunkley16 <hvgdunkley@gmail.com>
2023-03-13 14:57:35 -0400

    added photo, typing javascript animation, skill section

--- main.js
+++ main.js
@@ -1,87 +1,49 @@
-var content = [
-    "Smith College student",
-    "Musician",
-    "Programmer"
-]
+var mainbottom = $('.about-me-section').offset().top + $('.about-me-section').height();
 
-var part = 0;
+// on scroll, 
+$(window).on('scroll',function(){
 
-var part_index = 0;
+    // we round here to reduce a little workload
+    var stop = Math.round($(window).scrollTop());
 
-var interval_val;
+    if (stop > mainbottom) {
+        $('#logo').addClass('past-about');
+    } else {
+        $('#logo').removeClass('past-about');
+    }
 
-var element = document.querySelector("#text");
-var cursor = document.querySelector("#cursor");
-//typing effect
-function type(){
-    var text = content[part].substring(0, part_index + 1);
-    element.innerHTML = text;
-    part_index++;
+});
 
-    if(text === content[part]){
-        cursor.style.display = "none";
-        clearInterval(interval_val);
-        setTimeout(function(){
-            interval_val = setInterval(Delete, 50);
-        }, 1000);
+// Collapse Navbar
+var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  $('html, body').on('mousewheel', function() {
+    $('html, body').stop(); // Stops auto-scrolling upon manual scrolling
+  });
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector(".trigger");
+const closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
     }
 }
-function Delete() {
-	// Get substring with 1 characater deleted
-	var text =  content[part].substring(0, part_index - 1);
-	element.innerHTML = text;
-	part_index--;
 
-
-    if(text === '') {
-		clearInterval(interval_val);
-
-		// If current sentence was last then display the first one, else move to the next
-		if(part == (content.length - 1))
-			part = 0;
-		else
-			part++;
-		
-		part_index = 0;
-
-		// Start to display the next sentence after some time
-		setTimeout(function() {
-			cursor.style.display = 'inline-block';
-			interval_val = setInterval(Type, 100);
-		}, 200);
-	}
-}
-
-// Start the typing effect on load
-interval_val = setInterval(Type, 100);
-
-
-
-window.onscroll = function() {myFunction()};
-  
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
-
-function myFunction() {
-if (window.pageYOffset > sticky) {
-header.classList.add("sticky");
-} else {
- header.classList.remove("sticky");
-	}
-}
-var coll = document.getElementsByClassName("collapsible");
-var i;
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-	this.classList.toggle("active");
-	var content = this.nextElementSibling;
-	if (content.style.display === "block") {
-	  content.style.display = "none";
-	} else {
-	  content.style.display = "block";
-	}
-});
-(".front").click(function(){
-(this).toggleClass("back")  ; 
-})
-}
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
